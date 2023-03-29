09d82dd347717c4eee4a83f18ef7a1f7e24c00e4
dongurame <sanulim94@gmail.com>
2023-03-29 18:29:29 +0900

    starbucks

--- js/main.js
+++ js/main.js
@@ -49,3 +49,21 @@ new Swiper(".notice-line .swiper-container", {
   autoplay: true,
   loop: true,
 });
+
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 800,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true, //사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
