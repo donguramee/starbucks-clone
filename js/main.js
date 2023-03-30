36ad8d1f9a7ad2d8266d8547149dd96d2136d9ce
dongurame <sanulim94@gmail.com>
2023-03-31 02:14:56 +0900

    starbucks project-finish

--- js/main.js
+++ js/main.js
@@ -15,24 +15,43 @@ searchInputEl.addEventListener("blur", function () {
   searchInputEl.setAttribute("placeholder", "");
 });
 const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
+
 window.addEventListener(
   "scroll",
   _.throttle(function () {
     console.log(window.scrollY);
     if (window.scrollY > 500) {
+      // 배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션);
       gsap.to(badgeEl, 0.6, {
         opacity: 0,
         display: "none",
       });
+      // 버튼보이기
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
     } else {
+      // 배지 보이기
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: "block",
+      });
+      // 버튼 숨기기!!
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
       });
     }
   }, 300)
 );
 // _.throttle(함수, 시간)
+
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
 
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
@@ -67,6 +86,16 @@ new Swiper(".promotion .swiper-container", {
     nextEl: ".promotion .swiper-next",
   },
 });
+new Swiper(".awards .swiper-container", {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next",
+  },
+});
 
 const promotionEl = document.querySelector(".promotion");
 const promotionToggleBtn = document.querySelector(".toggle-promotion");
@@ -81,3 +110,41 @@ promotionToggleBtn.addEventListener("click", function () {
     //보임처리
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), //애니메이션 동작 시간
+    {
+      //옵션
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: random(0, delay),
+    }
+  );
+}
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 20);
+floatingObject(".floating3", 1.5, 20);
+
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, //보여짐 여부를 감시할 요소를 적용
+    triggerHook: 0.8,
+  })
+    .setClassToggle(spyEl, "show")
+    .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear(); //2023
---
+++ js/youtube.js
@@ -0,0 +1,25 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() {
+  // <div id='player'></div>
+  new YT.Player("player", {
+    videoId: "An6LvWQuj_8",
+    playerVars: {
+      autoplay: true,
+      loop: true,
+      playlist: "An6LvWQuj_8",
+    },
+    events: {
+      onReady: function (event) {
+        event.target.mute(); //음소거
+      },
+    },
+  });
+}
